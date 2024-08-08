c15d9495472b519cdd59b47977934dc43df98e6a
Marco Caessens <marco@keypoint.be>
2024-08-08 23:25:27 +0200

    Change styling

--- data/books.js
+++ data/books.js
@@ -1,8 +1,7 @@
 const books = [
     {
         title: "Owlbound by Winters's Beak",
-        genre: "Young Adult Fantasy",
-        release: "September 2024",
+        tags: ["Young Adult Fantasy", "Release: September 2024"],
         description: [
             "Tyrus grew up in a world without seasons, only snow and ice. A world, where humans and elves used to get along. A world, where fiery magic once thrived through a connection between owls and people. A world, he is destined to save.",
             "Long ago, a never-ending winter conquered all of the Wingspan Continent, and any hopes of that ever changing, had died silent deaths. But when Tyrus bonds with a curious owl that shouldn't exist, all those hopes are resurrected. He becomes the first Owlbound mage in over a century, capable of wielding the magical flames of legend and myth.",
@@ -12,8 +11,7 @@ const books = [
     },
     {
         title: "Owlbound #2",
-        genre: "Young Adult Fantasy",
-        release: "To Be Announced",
+        tags: ["Young Adult Fantasy", "Release: To Be Announced"],
         description: [
             "This novel follows Tyrus and his friends after the events of the first novel as their adventures continue.",
             "This novel is still a work in progress and in the drafting process.",
@@ -22,8 +20,7 @@ const books = [
     },
     {
         title: "Project Inkblood",
-        genre: "New Adult Fantasy",
-        release: "To Be Announced",
+        tags: ["New Adult Fantasy", "Release: To Be Announced"],
         description: ["This novel is still a work in progress and in the early plotting stages"],
         imageName: "Inkblood.png",
     },
--- scripts/bookLogic.js
+++ scripts/bookLogic.js
@@ -25,6 +25,18 @@ const createBookElements = () => {
         const cardImage = bookTemplate.content.querySelector("img.card-img-top");
         cardImage.src = `./images/${book.imageName}`;
 
+        const tagContainer = Object.assign(document.createElement("p"), { className: "card-text" });
+        book.tags.forEach((tag, index) => {
+            var className = "badge rounded-pill bg-secondary px-3";
+            if (index !== 0) className += " mx-1";
+            const tagEl = Object.assign(document.createElement("span"), {
+                className,
+            });
+            tagEl.textContent = tag;
+            tagContainer.append(tagEl);
+        });
+        cardBody.append(tagContainer);
+
         book.description.forEach((bookDescription) => {
             const descriptionEl = document.createElement("p");
             descriptionEl.className = "card-text";
